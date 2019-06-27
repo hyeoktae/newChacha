f838f98a6c21f353cc2bdb076fc483fa7f8a7455
hyeoktae kwon <kht_0218@hanmail.net>
2019-06-28 02:26:13 +0900

    document: tass - add 2 more data model

---
+++ chacha/Data/Fury.swift
@@ -0,0 +1,9 @@
+//
+//  Fury.swift
+//  chacha
+//
+//  Created by hyeoktae kwon on 2019/06/28.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import Foundation
---
+++ chacha/Data/chacha.swift
@@ -0,0 +1,9 @@
+//
+//  chacha.swift
+//  chacha
+//
+//  Created by hyeoktae kwon on 2019/06/28.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import Foundation
